b14376501ef6c734f35243f6b91025825fb2055a
tiagosb <tdsb.contato@gmail.com>
2020-10-30 19:34:50 -0300

    Implementa uma exemplo de busca ordenada no arquivo busca1.py

--- busca1.py
+++ busca1.py
@@ -6,7 +6,7 @@
 #
 # A função busca1 faz duas verificações a cada execução: i < len(lista) e lista[i] == alvo;
 # A função busca2 faz apenas uma verificação por execução: lista[i] == alvo
-#
+# A função busca3 busca na lista ordenada
 # ---------------------------------------------------------------------------------------
 import time
 import random
@@ -29,13 +29,27 @@ def busca2(alvo):
     i = 0
     while not lista[i] == alvo:
         i += 1
+    lista.pop() #Remove o elemento que havia sido adicionado
     if not i == len(lista) - 1:
         return i  # Item encontrado; retorna o indice
     return None
 
+def busca3(alvo):
+	lista.sort()
+	lista.append(alvo) #Adiciona o alvo no final da lista
+	i = 0
+	while lista[i] < alvo:
+		i += 1
+	if i == len(lista)-1 or not lista[i] == alvo:
+		lista.pop()
+		return None
+	lista.pop()
+	return i
 
-for i in range(1000000):
-    lista.append(random.randint(10, 100000))
+
+
+for i in range(10000):
+    lista.append(random.randint(10, 1000))
 
 n = random.choice(lista)
 
@@ -54,9 +68,8 @@ start2 = time.time()
 busca2(n)
 tempo2 = time.time() - start2
 print("Busca2     -> {}".format(tempo2))
-print("Diferenca  -> {}\n".format(tempo1 - tempo2))
 
-if tempo1 > tempo2:
-    print("A segunda funcao foi mais rapida que a primeira.")
-else:
-    print("A primeira funcao foi mais rapida que a primeira.")
+start3 = time.time()
+busca3(n)
+tempo3 = time.time() - start3
+print("Busca3     -> {}".format(tempo3))
